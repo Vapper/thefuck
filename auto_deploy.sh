02fe13e7421f411237f02aa5ed851e7bff565a83
Indrek Klanberg <indrek@unionfintech.com>
2018-06-18 16:22:52 +0200

    fix auto_deploy

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $TRAVIS_BRANCH == 'master' ]]
+if [[ $TRAVIS_BRANCH == 'development' ]]
     curl -o /tmp/travis-automerge https://raw.githubusercontent.com/cdown/travis-automerge/master/travis-automerge
     chmod a+x /tmp/travis-automerge
     BRANCHES_TO_MERGE_REGEX='development' BRANCH_TO_MERGE_INTO='master' GITHUB_REPO=sjoerdwels/thefuck /tmp/travis-automerge
