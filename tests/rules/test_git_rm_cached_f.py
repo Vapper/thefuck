e2ffb0c7a87fc6577d1f8944d5019a61d353d5ae
Indrek Klanberg <indrek@unionfintech.com>
2018-06-21 12:04:47 +0200

    Some changes missing from last commit.

--- tests/rules/test_git_rm_cached_f.py
+++ tests/rules/test_git_rm_cached_f.py
@@ -1,5 +1,5 @@
 import pytest
-from thefuck.rules.git_rm_local_modifications import match, get_new_command
+from thefuck.rules.git_rm_cached_f import match, get_new_command
 from thefuck.types import Command
 
 
