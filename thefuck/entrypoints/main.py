8745dbd69ba5ce0de6e4230250bebf1adbb1f66c
Silver Vapper <silvervapper@live.com>
2018-06-21 12:50:38 +0200

    check for passing os.getuid on Windows

--- thefuck/entrypoints/main.py
+++ thefuck/entrypoints/main.py
@@ -3,6 +3,7 @@ from ..system import init_output
 
 init_output()
 
+import platform
 import os  # noqa: E402
 import sys  # noqa: E402
 from .. import logs  # noqa: E402
@@ -12,7 +13,8 @@ from .alias import print_alias  # noqa: E402
 from .fix_command import fix_command  # noqa: E402
 
 global uid
-uid = os.getuid()
+if(platform.system()!="Windows"):
+    uid = os.getuid()
 
 
 def getUID():
